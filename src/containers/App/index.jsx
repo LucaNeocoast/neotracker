8dfa6fdc0a17e17e0a5d84d5bc6d30e48785ba6e
NeoCoast <neocoast@Mac-mini-de-NeoCoast.local>
2022-03-08 16:04:19 -0300

    setup completo

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { ROUTES } from 'Data/constants';
+import Layout from 'Components/Layout';
+import Home from 'Containers/Home';
+import AboutUs from 'Containers/AboutUs';
+import NotFound from 'Containers/NotFound';
+
+import './index.scss';
+
+const App = () => (
+  <Router>
+    <Layout>
+      <Routes>
+        <Route exact path={ROUTES.home} component={Home} />
+        <Route exact path={ROUTES.about} component={AboutUs} />
+        <Route component={NotFound} />
+      </Routes>
+    </Layout>
+  </Router>
+);
+
+export default App;
