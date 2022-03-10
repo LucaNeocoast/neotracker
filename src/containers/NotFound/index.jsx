829a8d45f7e7eea5557b6132256eb2f3ac719f53
NeoCoast <neocoast@Mac-mini-de-NeoCoast.local>
2022-03-10 15:29:18 -0300

    Sign In con todas las funcionalidades

--- src/containers/NotFound/index.jsx
+++ src/containers/NotFound/index.jsx
@@ -5,9 +5,6 @@ import './index.scss';
 function NotFound() {
   return (
     <div className="not-found">
-      <h1>
-        Oops <span role="img" aria-label="crying face">😭</span>
-      </h1>
       <h5>
         Boilerplate made with <span role="img" aria-label="love">♥️</span> by <a href="https://www.neocoast.com" target="_blank" rel="nofollow">NeoCoast</a>
       </h5>
