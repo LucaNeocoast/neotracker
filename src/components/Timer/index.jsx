4701793c1e9f8afbae11f8fef477a3f43a770f90
NeoCoast <neocoast@Mac-mini-de-NeoCoast.local>
2022-03-16 16:20:49 -0300

    reports y timer listos

---
+++ src/components/Timer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.scss';
+function timer() {
+  return (
+    <div className="timer">
+      soy el timer <span className="timer__wi">WIIIIIIIIIIIIIIIIIIII</span>!!!!!!
+    </div>
+  );
+}
+
+export default timer;
