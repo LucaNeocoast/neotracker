76bf33f4d7e11f39dd82ce25741936e373edb846
NeoCoast <neocoast@Mac-mini-de-NeoCoast.local>
2022-03-24 17:09:18 -0300

    terminado el historial de entradas

--- src/components/Entries/index.jsx
+++ src/components/Entries/index.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { format } from 'date-fns';
+import { format, differenceInMinutes } from 'date-fns';
+import { minutesToHours } from 'date-fns/esm';
 
 import PropTypes from 'prop-types';
 
-const Entries = ({ description, finishDate, initialDate }) => (
-  <div className="project">
-    <div className="project__date">{format(new Date(initialDate), 'hh:mm a')}</div>
-    <div className="project__date">{format(new Date(finishDate), 'hh:mm a')}</div>
-    <div className="project__line">{description}</div>
-  </div>
-);
+import './index.scss';
 
+const Entries = ({ description, finishDate, initialDate }) => {
+  const [minutes, setMinutes] = useState('');
+  const [diference, setDiference] = useState('');
+
+  useEffect(() => {
+    setDiference(differenceInMinutes(new Date(finishDate), new Date(initialDate)));
+    setMinutes(Math.round(diference - (60 * (minutesToHours(diference)))));
+  }, [diference]);
+  return (
+
+    <div className="entries">
+      <div className="entries__name">{description}</div>
+      <div className="entries__container">
+        <div className="entries__container-time">{format(new Date(initialDate), 'hh:mm a')} - </div>
+        <div className="entries__container-time">{format(new Date(finishDate), 'hh:mm a')}</div>
+        <div className="entries__container-total">{diference > 0 ? `${minutesToHours(diference)}:${minutes}` : '0:0'}</div>
+      </div>
+    </div>
+  );
+};
 Entries.propTypes = {
   description: PropTypes.string.isRequired,
   finishDate: PropTypes.string.isRequired,
